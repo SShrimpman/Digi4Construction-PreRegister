ba997c160059ab7d5802c2502d6d8b0e628a27b6
António Camarão <camaraoacsn@gmail.com>
2023-05-18 12:38:21 +0100

    Added Javascript code that was missing

--- resources/views/components/form/form.blade.php
+++ resources/views/components/form/form.blade.php
@@ -86,3 +86,43 @@
         </div>
     </section>
 </form>
+
+<script>
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myButton");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    var state = false;
+
+    function toggle() {
+        if (state) {
+            document.getElementById("password").setAttribute("type", "password");
+            state = false;
+        } else {
+            document.getElementById("password").setAttribute("type", "text");
+            state = true;
+        }
+    }
+</script>
