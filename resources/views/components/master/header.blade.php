1f5241bb6aaa32c322932f066caefa054f7587a9
António Camarão <camaraoacsn@gmail.com>
2023-05-18 16:55:21 +0100

    Header CSS changed completely to use Tailwind

--- resources/views/components/master/header.blade.php
+++ resources/views/components/master/header.blade.php
@@ -1,11 +1,14 @@
-<section class="grid-container">
-    <section class="column-span-4 header-info-container">
-        <div class="header-grid">
-            <div class="header-image">
-                <img src="/logoms.svg" alt="MS" class="ms-logo">
-            </div>
-            <div class="header-text">
-                <h1>Pre Register</h1>
+    <div class="flex justify-center">
+        <div class="bg-backgroundHeader mt-5 w-800 p-4 rounded-t-2xl">
+            <div class="flex justify-center">
+                <div class="grid grid-cols-2">
+                    <div class="ml-10">
+                        <img src="/logoms.svg" class="w-44">
+                    </div>
+                    <div class="flex justify-center">
+                        <h1 class="grid content-center text-white text-4xl font-bold">Pre Register</h1>
+                    </div>
+                </div>
             </div>
         </div>
-    </section>
+    </div>
