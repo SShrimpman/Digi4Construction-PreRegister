5431c2b83eb4dc0a821c608c890dd6d4fa8bcc3e
António Camarão <camaraoacsn@gmail.com>
2023-05-18 12:15:17 +0100

    Added code to the Header/Main/Footer components

--- resources/views/components/master/header.blade.php
+++ resources/views/components/master/header.blade.php
@@ -1,11 +1,6 @@
-<section class="column-span-4 header-info-container">
-    <img src="/logoms.svg" alt="MS" class="ms-logo">
-    <h1>Pre Register</h1>
-    <!-- <p>Become eligible to be selected as a privileged user</p> -->
-</section>
-<div class="column-span-4 joinus-wrapper">
-    <h3>Join Us Now</h3>
-    <!-- <p>Please fill out this form</p> -->
-    <p style="font-size: 14px;">Become eligible to be selected as a privileged user!</p>
-    <hr>
-</div>
+<section class="grid-container">
+    <section class="column-span-4 header-info-container">
+        <img src="/logoms.svg" alt="MS" class="ms-logo">
+        <h1>Pre Register</h1>
+        <!-- <p>Become eligible to be selected as a privileged user</p> -->
+    </section>
--- resources/views/components/master/main.blade.php
+++ resources/views/components/master/main.blade.php
@@ -1,30 +1,33 @@
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+<!DOCTYPE html>
 
-    <title>MS Form</title>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
-    <!-- Fonts and Icons -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="/style.css">
-    <link rel="icon" type="image/x-icon" href="/logoms.svg">
+        <title>MS Form</title>
 
-    <!-- Styles -->
-    <link rel="stylesheet" type="text/css" href="/style.css">
-</head>
-<body>
-@component('components.master.header')
-@endcomponent
+        <!-- Fonts and Icons -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="/style.css">
+        <link rel="icon" type="image/x-icon" href="/logoms.svg">
 
-<main>
-    @yield('content')
-</main>
+        <!-- Styles -->
+        <link rel="stylesheet" type="text/css" href="/style.css">
+    </head>
+    <body>
+        <main class="page-wrapper">
+            @component('components.master.header')
+            @endcomponent
 
-@component('components.master.footer')
-@endcomponent
-</body>
+            @yield('content')
+            
+            @component('components.master.footer')
+            @endcomponent
+        </main>
+    </body>
+</html>
