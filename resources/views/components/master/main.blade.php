ef6e9ffc3c73ed3d5da4ad9ab02bb4bf1f8a7ccc
António Camarão <camaraoacsn@gmail.com>
2023-05-18 16:55:52 +0100

    Main blade uses app.css

--- resources/views/components/master/main.blade.php
+++ resources/views/components/master/main.blade.php
@@ -17,12 +17,12 @@
         <link rel="icon" type="image/x-icon" href="/logoms.svg">
 
         <!-- Styles -->
-        {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        @yield('styles') --}}
-        <link rel="stylesheet" type="text/css" href="/style.css">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        @yield('styles')
+        {{-- <link rel="stylesheet" type="text/css" href="/style.css"> --}}
     </head>
     <body>
-        <main class="page-wrapper">
+        {{-- <main class="page-wrapper"> --}}
             @component('components.master.header')
             @endcomponent
 
@@ -30,6 +30,6 @@
             
             @component('components.master.footer')
             @endcomponent
-        </main>
+        {{-- </main> --}}
     </body>
 </html>
