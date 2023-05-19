e3950353ce1e09222dbb93581876c7edb1e42b88
António Camarão <camaraoacsn@gmail.com>
2023-05-19 16:29:18 +0100

    Custom CSS added to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,26 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'betatron': ['Betatron', 'sans-serif'],
+      },
+      borderWidth: {
+        '1': '1px',
+      },
       colors: {
         'backgroundHeader': '#152243',
+        'lightBlue': '#00a2ff',
+        'transparentBg': 'rgba(0,0,0,.4)',
+      },
+      margin: {
+        '355': '355px',
       },
       width: {
         '800': '800px',
+        '1000': '1000px'
+      },
+      height: {
+        '500': '500px',
       }
     },
   },
