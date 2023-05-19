6f7607f3c018f9ea6caca976da9da4b22de68ee6
António Camarão <camaraoacsn@gmail.com>
2023-05-19 16:59:26 +0100

    Applied CSS Typing effect to Success page

--- resources/views/components/success/success.blade.php
+++ resources/views/components/success/success.blade.php
@@ -1,31 +1,6 @@
-<div class="column-span-4 joinus-wrapper">
+<div class="wrapper">
+    <div class="typing-demo">
+      Obrigado pelo seu registo.
+      O Digi4Construction espera por si!
+    </div>
 </div>
-
-<div class="column-span-4 joinus-wrapper">
-</div>
-
-<section class="column-span-4 alert-sucess">
-    <div class="sucess-logo">
-        <img src="/sucess.svg" class="sucesso">
-    </div>
-</section>
-
-<section class="column-span-4 footer-wrapper">
-    <div class="footer-item">
-        <p class="footer-text">UNIFY</p>
-        <img class="footer-image" src="/unify.png" alt="">
-       
-    </div>
-    <div class="footer-item">
-        <p class="footer-text">SIMPLIFY</p>
-        <img class="footer-image" src="/simplify.png" alt="">
-       
-    </div>
-    <div class="footer-item">
-        <p class="footer-text">AMPLIFY</p>
-        <img class="footer-image" src="/amplify.png" alt="">
-    </div>
-   
-</section>
-
-
