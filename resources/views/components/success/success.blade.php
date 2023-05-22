44107a350a84a8c4dc0ed4728e221084e8343d37
António Camarão <camaraoacsn@gmail.com>
2023-05-22 12:08:13 +0100

    Success page is now responsive and finalized

--- resources/views/components/success/success.blade.php
+++ resources/views/components/success/success.blade.php
@@ -1,6 +1,30 @@
-<div class="wrapper">
-    <div class="typing-demo">
+<div class="hidden md:block md:grid md:content-center">
+  <div id="wrapper">
+    <div id="typing-demo1">
       Obrigado pelo seu registo.
+    </div>
+  </div>
+  <div id="wrapper">
+    <div id="typing-demo2">
       O Digi4Construction espera por si!
     </div>
+  </div>
+</div>
+
+<div class="grid content-center md:hidden lg:hidden">
+  <div id="wrapper">
+    <div id="typing-demo3">
+      Obrigado pelo seu registo.
+    </div>
+  </div>
+  <div id="wrapper">
+    <div id="typing-demo4">
+      O Digi4Construction espera
+    </div>
+  </div>
+  <div id="wrapper">
+    <div id="typing-demo5">
+      por si!
+    </div>
+  </div>
 </div>
