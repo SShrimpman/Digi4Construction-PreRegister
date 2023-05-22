52c3cd51b824bbc0dc5d8c6a4ebe237a61e72065
António Camarão <camaraoacsn@gmail.com>
2023-05-22 08:42:20 +0100

    Corrected Icon for showing password

--- resources/views/components/form/form.blade.php
+++ resources/views/components/form/form.blade.php
@@ -1,4 +1,4 @@
-<div class="flex items-center">
+<div class="md:flex items-center">
     <div class="lg:mt-5 w-800 lg:rounded-t-2xl">
             <div class="flex justify-center">
                 <h1 class="text-white text-4xl p-5 lg:text-5xl font-bold font-betatron tracking-widest">
@@ -71,7 +71,7 @@
                     <div>
                         <div class="relative h-10 w-full min-w-[200px]">
                             <div class="absolute top-2/4 right-3 grid h-5 w-5 -translate-y-2/4 place-items-center text-blue-gray-500">
-                                <i class="fa fa-eye" aria-hidden="true" id="eye" onclick="toggle()"></i>
+                                <i class="fa fa-eye-slash" aria-hidden="true" id="eye" onclick="toggle()"></i>
                             </div>
                             <input id="password" type="password"
                               class="peer h-full w-full rounded-[7px] border border-blue-gray-200 bg-transparent px-3 py-2.5 !pr-9 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-1 focus:border-lightBlue focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
